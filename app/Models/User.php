6e2fdbe665642ba169b62968d87a0c6e1810d752
Burton Johnsey <git@tonjohn.com>
2020-11-25 13:44:58 -0800

    update user with temp homes relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,4 +57,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function homes()
+    {
+        return $this->hasMany(UserHome::class, 'created_by');
+    }
 }
